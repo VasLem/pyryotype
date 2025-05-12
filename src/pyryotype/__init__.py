272fde7bab764099ac2daebf5321b137697179aa
vlemonidis <vasileios.lemonidis@uantwerpen.be>
2025-05-12 17:24:32 +0200

    Add make_ideogram_grid function and update plot_ideogram parameters

--- src/pyryotype/__init__.py
+++ src/pyryotype/__init__.py
@@ -2,13 +2,14 @@
 #
 # SPDX-License-Identifier: MIT
 from pyryotype.coverage import plot_coverage
-from pyryotype.ideogram import GENOME, plot_ideogram
+from pyryotype.ideogram import GENOME, plot_ideogram, make_ideogram_grid
 from pyryotype.paf_plotting import PAFProtocol, PlotMode, plot_paf_alignments
 from pyryotype.paf_tools import get_metadata_csv
 
 __all__ = [
     'GENOME',
     'plot_ideogram',
+    "make_ideogram_grid",
     'plot_coverage',
     'plot_paf_alignments',
     'PAFProtocol',
